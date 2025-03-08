209d1cb8f11e8b025cf4b3d1e571a1b9eef7b217
Lorofer <Lev.buben@gmail.com>
2025-03-08 12:33:45 +0300

    добавление маршрутизации, верстка страницы авторизации

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
 import { useState } from 'react'
-import './App.scss'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import {tasks} from '@/data.js'
-import Dashboard from '@/components/Dashboard/Dashboard.jsx'
+import { tasks } from '@/data.js'
+import Dashboard from '@/pages/Dashboard/Dashboard.jsx'
+import SignUp from '@/pages/SignUp/SignUp.jsx'
+
+import './App.scss'
 
 function App() {
     const [count, setCount] = useState(0)
 
     return (
         <>
-            <Dashboard tasks={tasks}/>
+            <BrowserRouter>
+                <Routes>
+                    <Route
+                        path={'/'}
+                        element={<Dashboard tasks={tasks} />}
+                    />
+                    <Route
+                        path={'/signup'}
+                        element={<SignUp />}
+                    />
+                </Routes>
+            </BrowserRouter>
         </>
     )
 }
